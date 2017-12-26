[modify]     G of gumbel-softmax GAN (implemented in LSTM)     Reward function of gumbel-softmax GAN [fix]     minor bug in disdataloader

models/gsgan/Gsgan.py
@@ -1,19 +1,21 @@
+import json
 from time import time
-
-from utils.metrics.Bleu import Bleu
-from utils.metrics.Nll import Nll
 
 from models.Gan import Gan
 from models.gsgan.GsganDataLoader import DataLoader, DisDataloader
 from models.gsgan.GsganDiscriminator import Discriminator
 from models.gsgan.GsganGenerator import Generator
 from models.gsgan.GsganReward import Reward
+from utils.metrics.Bleu import Bleu
+from utils.metrics.Cfg import Cfg
 from utils.metrics.EmbSim import EmbSim
+from utils.oracle.OracleCfg import OracleCfg
 from utils.oracle.OracleLstm import OracleLstm
+from utils.text_process import *
 from utils.utils import *
 
 
-class Seqgan(Gan):
+class Gsgan(Gan):
     def __init__(self, oracle=None):
         super().__init__()
         # you can change parameters, generator here
@@ -21,8 +23,8 @@ class Seqgan(Gan):
         self.emb_dim = 32
         self.hidden_dim = 32
         self.sequence_length = 20
-        self.filter_size = [2, 3]
-        self.num_filters = [100, 200]
+        self.filter_size = [3, 4, 5]
+        self.num_filters = 128
         self.l2_reg_lambda = 0.2
         self.dropout_keep_prob = 0.75
         self.batch_size = 64
@@ -31,7 +33,9 @@ class Seqgan(Gan):
 
         self.oracle_file = 'save/oracle.txt'
         self.generator_file = 'save/generator.txt'
+        self.test_file = 'save/test_file.txt'
 
+    def init_oracle_trainng(self, oracle=None):
         if oracle is None:
             oracle = OracleLstm(num_vocabulary=self.vocab_size, batch_size=self.batch_size, emb_dim=self.emb_dim,
                                 hidden_dim=self.hidden_dim, sequence_length=self.sequence_length,
@@ -43,8 +47,10 @@ class Seqgan(Gan):
                               start_token=self.start_token)
         self.set_generator(generator)
 
+        # fixme bad taste
         discriminator = Discriminator(sequence_length=self.sequence_length, num_classes=2, vocab_size=self.vocab_size,
-                                      emd_dim=self.emb_dim, filter_sizes=self.filter_size, num_filters=self.num_filters,
+                                      hidden_unit=20, embedding_size=self.emb_dim, filter_sizes=self.filter_size,
+                                      num_filters=self.num_filters, non_static=True,
                                       l2_reg_lambda=self.l2_reg_lambda)
         self.set_discriminator(discriminator)
 
@@ -64,24 +70,35 @@ class Seqgan(Gan):
         embsim = EmbSim(model=self)
         self.add_metric(embsim)
 
-        nll = Nll(data_loader=self.oracle_data_loader, rnn=self.oracle, sess=self.sess)
-        self.add_metric(nll)
-
-        inll = Nll(data_loader=self.gen_data_loader, rnn=self.generator, sess=self.sess)
-        inll.set_name('i-nll')
-        self.add_metric(inll)
+        # nll = Nll(data_loader=self.oracle_data_loader, rnn=self.oracle, sess=self.sess)
+        # self.add_metric(nll)
+        #
+        # inll = Nll(data_loader=self.gen_data_loader, rnn=self.generator, sess=self.sess)
+        # inll.set_name('i-nll')
+        # self.add_metric(inll)
 
     def train_discriminator(self):
+
+        def real_len(batches):
+            return [np.ceil(np.argmin(batch + [0]) * 1.0 / 4) for batch in batches]
+
         generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
         self.dis_data_loader.load_train_data(self.oracle_file, self.generator_file)
         for _ in range(3):
-            self.dis_data_loader.reset_pointer()
+            self.dis_data_loader.next_batch()
             x_batch, y_batch = self.dis_data_loader.next_batch()
             feed = {
                 self.discriminator.input_x: x_batch,
                 self.discriminator.input_y: y_batch,
+                self.discriminator.dropout_keep_prob: 0.8,
+                self.discriminator.batch_size: len(x_batch),
+                self.discriminator.pad: np.zeros([len(x_batch), 1, 32, 1]),
+                self.discriminator.real_len: real_len(x_batch),
             }
-            _ = self.sess.run(self.discriminator.train_op, feed)
+            # _ = self.sess.run(self.discriminator.train_op, feed)
+            _, step, loss, accuracy = self.sess.run(
+                [self.discriminator.train_op, self.discriminator.global_step, self.discriminator.loss,
+                 self.discriminator.accuracy], feed)
 
     def evaluate(self):
         generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
@@ -89,12 +106,13 @@ class Seqgan(Gan):
         if self.log is not None:
             scores = super().evaluate()
             for score in scores:
-                self.log.write(str(score)+',')
+                self.log.write(str(score) + ',')
             self.log.write('\n')
             return scores
         return super().evaluate()
 
     def train_oracle(self):
+        self.init_oracle_trainng()
         self.init_metric()
         self.sess.run(tf.global_variables_initializer())
 
@@ -126,7 +144,111 @@ class Seqgan(Gan):
 
         self.reset_epoch()
         print('adversarial training:')
-        self.reward = Reward(self.generator, .8)
+        self.reward = Reward()
+        for epoch in range(self.adversarial_epoch_num):
+            print('epoch:' + str(epoch))
+            start = time()
+            for index in range(1):
+                samples = self.generator.generate(self.sess)
+                rewards = self.reward.get_reward(self.sess, samples, 16, self.discriminator)
+                z_h0 = np.random.uniform(low=0, high=1, size=[self.batch_size, self.emb_dim])
+                z_c0 = np.random.uniform(low=0, high=1, size=[self.batch_size, self.emb_dim])
+
+                feed = {
+                    self.generator.x: samples,
+                    self.generator.rewards: rewards,
+                    self.generator.h_0: z_h0,
+                    self.generator.c_0: z_c0,
+                }
+                _ = self.sess.run(self.generator.g_updates, feed_dict=feed)
+            end = time()
+            self.add_epoch()
+            print('epoch:' + str(epoch) + '\t time:' + str(start - end))
+            if epoch % 5 == 0 or epoch == self.adversarial_epoch_num - 1:
+                generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
+                self.evaluate()
+
+            for _ in range(15):
+                self.train_discriminator()
+
+    def init_cfg_training(self, grammar=None):
+        oracle = OracleCfg(sequence_length=self.sequence_length, cfg_grammar=grammar)
+        self.set_oracle(oracle)
+        self.oracle.generate_oracle()
+        self.vocab_size = self.oracle.vocab_size + 1
+
+        generator = Generator(num_vocabulary=self.vocab_size, batch_size=self.batch_size, emb_dim=self.emb_dim,
+                              hidden_dim=self.hidden_dim, sequence_length=self.sequence_length,
+                              start_token=self.start_token)
+        self.set_generator(generator)
+
+        discriminator = Discriminator(sequence_length=self.sequence_length, num_classes=2, vocab_size=self.vocab_size,
+                                      hidden_unit=20, embedding_size=self.emb_dim, filter_sizes=self.filter_size,
+                                      num_filters=self.num_filters, non_static=True,
+                                      l2_reg_lambda=self.l2_reg_lambda)
+        self.set_discriminator(discriminator)
+
+        gen_dataloader = DataLoader(batch_size=self.batch_size, seq_length=self.sequence_length)
+        oracle_dataloader = DataLoader(batch_size=self.batch_size, seq_length=self.sequence_length)
+        dis_dataloader = DisDataloader(batch_size=self.batch_size, seq_length=self.sequence_length)
+        self.set_data_loader(gen_loader=gen_dataloader, dis_loader=dis_dataloader, oracle_loader=oracle_dataloader)
+        return oracle.wi_dict, oracle.iw_dict
+
+    def init_cfg_metric(self, grammar=None):
+        cfg = Cfg(test_file=self.test_file, cfg_grammar=grammar)
+        self.add_metric(cfg)
+
+    def train_cfg(self):
+        cfg_grammar = """
+          S -> S PLUS x | S SUB x |  S PROD x | S DIV x | x
+          PLUS -> '+'
+          SUB -> '-'
+          PROD -> '*'
+          DIV -> '/'
+          x -> 'x'
+        """
+
+        wi_dict_loc, iw_dict_loc = self.init_cfg_training(cfg_grammar)
+        with open(iw_dict_loc, 'r') as file:
+            iw_dict = json.load(file)
+
+        def get_cfg_test_file(dict=iw_dict):
+            with open(self.generator_file, 'r') as file:
+                codes = get_tokenlized(self.generator_file)
+            with open(self.test_file, 'w') as outfile:
+                outfile.write(code_to_text(codes=codes, dictionary=dict))
+
+        self.init_cfg_metric(grammar=cfg_grammar)
+        self.sess.run(tf.global_variables_initializer())
+
+        self.pre_epoch_num = 0
+        self.adversarial_epoch_num = 200
+        self.log = open('experiment-log-seqgan-cfg.csv', 'w')
+        # generate_samples(self.sess, self.oracle, self.batch_size, self.generate_num, self.oracle_file)
+        generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
+        self.gen_data_loader.create_batches(self.oracle_file)
+        self.oracle_data_loader.create_batches(self.generator_file)
+        print('start pre-train generator:')
+        for epoch in range(self.pre_epoch_num):
+            start = time()
+            loss = pre_train_epoch(self.sess, self.generator, self.gen_data_loader)
+            end = time()
+            print('epoch:' + str(epoch) + '\t time:' + str(start - end))
+            self.add_epoch()
+            if epoch % 5 == 0:
+                generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
+                get_cfg_test_file()
+                self.evaluate()
+
+        print('start pre-train discriminator:')
+        self.reset_epoch()
+        for epoch in range(self.pre_epoch_num):
+            print('epoch:' + str(epoch))
+            self.train_discriminator()
+
+        self.reset_epoch()
+        print('adversarial training:')
+        self.reward = Reward()
         for epoch in range(self.adversarial_epoch_num):
             print('epoch:' + str(epoch))
             start = time()
@@ -143,13 +265,14 @@ class Seqgan(Gan):
             print('epoch:' + str(epoch) + '\t time:' + str(start - end))
             if epoch % 5 == 0 or epoch == self.adversarial_epoch_num - 1:
                 generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
+                get_cfg_test_file()
                 self.evaluate()
 
-            self.reward.update_params()
             for _ in range(15):
                 self.train_discriminator()
 
 
 if __name__ == '__main__':
-    seqgan = Seqgan()
-    seqgan.train_oracle()
+    gan = Gsgan()
+    gan.train_oracle()
+    # gan.train_cfg()

models/gsgan/GsganDiscriminator.py
@@ -1,135 +1,112 @@
+import numpy as np
 import tensorflow as tf
 
 
-def linear(input_, output_size, scope=None):
-    '''
-    Linear map: output[k] = sum_i(Matrix[k, i] * input_[i] ) + Bias[k]
-    Args:
-    input_: a tensor or a list of 2D, batch x n, Tensors.
-    output_size: int, second dimension of W[i].
-    scope: VariableScope for the created subgraph; defaults to "Linear".
-  Returns:
-    A 2D Tensor with shape [batch x output_size] equal to
-    sum_i(input_[i] * W[i]), where W[i]s are newly created matrices.
-  Raises:
-    ValueError: if some of the arguments has unspecified or wrong shape.
-  '''
+class Discriminator():
+    def __init__(self, vocab_size, non_static, hidden_unit, sequence_length,
+                 num_classes, embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
+        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name='input_x')
+        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name='input_y')
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name='dropout_keep_prob')
+        self.batch_size = tf.placeholder(tf.int32, [])
+        self.pad = tf.placeholder(tf.float32, [None, 1, embedding_size, 1], name='pad')
+        self.real_len = tf.placeholder(tf.int32, [None], name='real_len')
 
-    shape = input_.get_shape().as_list()
-    if len(shape) != 2:
-        raise ValueError("Linear is expecting 2D arguments: %s" % str(shape))
-    if not shape[1]:
-        raise ValueError("Linear expects shape[1] of arguments: %s" % str(shape))
-    input_size = shape[1]
-
-    # Now the computation.
-    with tf.variable_scope(scope or "SimpleLinear"):
-        matrix = tf.get_variable("Matrix", [output_size, input_size], dtype=input_.dtype)
-        bias_term = tf.get_variable("Bias", [output_size], dtype=input_.dtype)
-
-    return tf.matmul(input_, tf.transpose(matrix)) + bias_term
-
-
-def highway(input_, size, num_layers=1, bias=-2.0, f=tf.nn.relu, scope='Highway'):
-    """Highway Network (cf. http://arxiv.org/abs/1505.00387).
-    t = sigmoid(Wy + b)
-    z = t * g(Wy + b) + (1 - t) * y
-    where g is nonlinearity, t is transform gate, and (1 - t) is carry gate.
-    """
-
-    with tf.variable_scope(scope):
-        for idx in range(num_layers):
-            g = f(linear(input_, size, scope='highway_lin_%d' % idx))
-
-            t = tf.sigmoid(linear(input_, size, scope='highway_gate_%d' % idx) + bias)
-
-            output = t * g + (1. - t) * input_
-            input_ = output
-
-    return output
-
-
-class Discriminator(object):
-    """
-    A CNN for text classification.
-    Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
-    """
-
-    def __init__(
-            self, sequence_length, num_classes, vocab_size,
-            emd_dim, filter_sizes, num_filters, l2_reg_lambda=0.0, dropout_keep_prob = 1):
-        # Placeholders for input, output and dropout
-        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
-        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
-        self.dropout_keep_prob = dropout_keep_prob
-        # self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
-
-        # Keeping track of l2 regularization loss (optional)
         l2_loss = tf.constant(0.0)
 
-        with tf.variable_scope('discriminator'):
-            # Embedding layer
-            with tf.device('/cpu:0'), tf.name_scope("embedding"):
-                self.W = tf.Variable(
-                    tf.random_uniform([vocab_size, emd_dim], -1.0, 1.0),
+        with tf.device('/cpu:0'), tf.name_scope('embedding'):
+            if not non_static:
+                self.W_ = tf.constant(
+                    tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
                     name="W")
-                self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
-                self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+            else:
+                self.W_ = tf.Variable(
+                    tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+                    name="W")
+            self.embedded_chars = tf.nn.embedding_lookup(self.W_, self.input_x)
+            emb = tf.expand_dims(self.embedded_chars, -1)
 
-            # Create a convolution + maxpool layer for each filter size
-            pooled_outputs = []
-            for filter_size, num_filter in zip(filter_sizes, num_filters):
-                with tf.name_scope("conv-maxpool-%s" % filter_size):
-                    # Convolution Layer
-                    filter_shape = [filter_size, emd_dim, 1, num_filter]
-                    W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                    b = tf.Variable(tf.constant(0.1, shape=[num_filter]), name="b")
-                    conv = tf.nn.conv2d(
-                        self.embedded_chars_expanded,
-                        W,
-                        strides=[1, 1, 1, 1],
-                        padding="VALID",
-                        name="conv")
-                    # Apply nonlinearity
-                    h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-                    # Maxpooling over the outputs
-                    pooled = tf.nn.max_pool(
-                        h,
-                        ksize=[1, sequence_length - filter_size + 1, 1, 1],
-                        strides=[1, 1, 1, 1],
-                        padding='VALID',
-                        name="pool")
-                    pooled_outputs.append(pooled)
+        pooled_concat = []
+        reduced = np.int32(np.ceil(sequence_length * 1.0 / (sequence_length - filter_sizes[0] + 1)))
 
-            # Combine all the pooled features
-            num_filters_total = sum(num_filters)
-            self.h_pool = tf.concat(pooled_outputs, 3)
-            self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+        for i, filter_size in enumerate(filter_sizes):
+            with tf.name_scope('conv-maxpool-%s' % filter_size):
+                # Zero paddings so that the convolution output have dimension batch x sequence_length x emb_size x channel
+                num_prio = (filter_size - 1) // 2
+                num_post = (filter_size - 1) - num_prio
+                pad_prio = tf.concat([self.pad] * num_prio, 1)
+                pad_post = tf.concat([self.pad] * num_post, 1)
+                emb_pad = tf.concat([pad_prio, emb, pad_post], 1)
 
-            # Add highway
-            with tf.name_scope("highway"):
-                self.h_highway = highway(self.h_pool_flat, self.h_pool_flat.get_shape()[1], 1, 0)
+                filter_shape = [filter_size, embedding_size, 1, num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name='W')
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name='b')
+                conv = tf.nn.conv2d(emb_pad, W, strides=[1, 1, 1, 1], padding='VALID', name='conv')
 
-            # Add dropout
-            with tf.name_scope("dropout"):
-                self.h_drop = tf.nn.dropout(self.h_highway, self.dropout_keep_prob)
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name='relu')
 
-            # Final (unnormalized) scores and predictions
-            with tf.name_scope("output"):
-                W = tf.Variable(tf.truncated_normal([num_filters_total, num_classes], stddev=0.1), name="W")
-                b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-                l2_loss += tf.nn.l2_loss(W)
-                l2_loss += tf.nn.l2_loss(b)
-                self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
-                self.ypred_for_auc = tf.nn.softmax(self.scores)
-                self.predictions = tf.argmax(self.scores, 1, name="predictions")
+                # Maxpooling over the outputs
+                max_pool_size = sequence_length - filter_size + 1
+                pooled = tf.nn.max_pool(h, ksize=[1, max_pool_size, 1, 1], strides=[1, max_pool_size, 1, 1],
+                                        padding='SAME', name='pool')
+                pooled = tf.reshape(pooled, [-1, reduced, num_filters])
+                pooled_concat.append(pooled)
 
-            # CalculateMean cross-entropy loss
-            with tf.name_scope("loss"):
-                losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
-                self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+        pooled_concat = tf.concat(pooled_concat, 2)
+        pooled_concat = tf.nn.dropout(pooled_concat, self.dropout_keep_prob)
 
-        self.params = [param for param in tf.trainable_variables() if 'discriminator' in param.name]
-        d_optimizer = tf.train.AdamOptimizer(1e-4)
-        grads_and_vars = d_optimizer.compute_gradients(self.loss, self.params, aggregation_method=2)
-        self.train_op = d_optimizer.apply_gradients(grads_and_vars)
+        lstm_cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_unit)
+
+        # lstm_cell = tf.nn.rnn_cell.GRUCell(num_units=hidden_unit)
+        # lstm_cell = tf.contrib.rnn.GRUCell(num_units=hidden_unit)
+
+        # lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=self.dropout_keep_prob)
+        lstm_cell = tf.contrib.rnn.DropoutWrapper(lstm_cell, output_keep_prob=self.dropout_keep_prob)
+
+        self._initial_state = lstm_cell.zero_state(self.batch_size, tf.float32)
+        # inputs = [tf.squeeze(input_, [1]) for input_ in tf.split(1, reduced, pooled_concat)]
+        inputs = [tf.squeeze(input_, [1]) for input_ in
+                  tf.split(pooled_concat, num_or_size_splits=int(reduced), axis=1)]
+        # outputs, state = tf.nn.rnn(lstm_cell, inputs, initial_state=self._initial_state,
+        # sequence_length=self.real_len)
+        outputs, state = tf.contrib.rnn.static_rnn(lstm_cell, inputs, initial_state=self._initial_state,
+                                                   sequence_length=self.real_len)
+
+        # Collect the appropriate last words into variable output (dimension = batch x embedding_size)
+        output = outputs[0]
+        with tf.variable_scope('Output'):
+            tf.get_variable_scope().reuse_variables()
+            one = tf.ones([1, hidden_unit], tf.float32)
+            for i in range(1, len(outputs)):
+                ind = self.real_len < (i + 1)
+                ind = tf.to_float(ind)
+                ind = tf.expand_dims(ind, -1)
+                mat = tf.matmul(ind, one)
+                output = tf.add(tf.multiply(output, mat), tf.multiply(outputs[i], 1.0 - mat))
+
+        with tf.name_scope('output'):
+            self.W = tf.Variable(tf.truncated_normal([hidden_unit, num_classes], stddev=0.1), name='W')
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name='b')
+            l2_loss += tf.nn.l2_loss(self.W)
+            l2_loss += tf.nn.l2_loss(b)
+            self.scores = tf.nn.xw_plus_b(output, self.W, b, name='scores')
+            self.ypred_for_auc = tf.nn.softmax(self.scores)
+            self.predictions = tf.argmax(self.scores, 1, name='predictions')
+
+        with tf.name_scope('loss'):
+            losses = tf.nn.softmax_cross_entropy_with_logits(labels=self.input_y,
+                                                             logits=self.scores)  # only named arguments accepted
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        with tf.name_scope('accuracy'):
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name='accuracy')
+
+        with tf.name_scope('num_correct'):
+            correct = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            self.num_correct = tf.reduce_sum(tf.cast(correct, 'float'))
+
+        self.global_step = tf.Variable(0, name='global_step', trainable=False)
+        optimizer = tf.train.RMSPropOptimizer(1e-3, decay=0.9)
+        grads_and_vars = optimizer.compute_gradients(self.loss)
+        self.train_op = optimizer.apply_gradients(grads_and_vars, global_step=self.global_step)

models/gsgan/GsganGenerator.py
@@ -1,6 +1,6 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops import tensor_array_ops, control_flow_ops
-import numpy as np
 
 
 class Generator(object):
@@ -59,7 +59,7 @@ class Generator(object):
             o_t = self.g_output_unit(h_t)  # batch x vocab , logits not prob
             h_plus_g = h_t[0] + self.get_gumbel(tf.shape(h_t[0]))
             x_tp1 = tf.nn.softmax(h_plus_g/self.tau)
-            next_token = tf.cast(tf.argmax(h_plus_g, axis=1), tf.int32)
+            next_token = tf.cast(tf.argmax(o_t, axis=1), tf.int32)
             gen_o = gen_o.write(i, tf.reduce_sum(tf.multiply(tf.one_hot(next_token, self.num_vocabulary, 1.0, 0.0),
                                                              tf.nn.softmax(o_t)), 1))  # [batch_size] , prob
             gen_x = gen_x.write(i, next_token)  # indices, batch_size
@@ -154,7 +154,14 @@ class Generator(object):
         return sess.run(self.pretrain_loss, feed)
 
     def pretrain_step(self, sess, x):
-        outputs = sess.run([self.pretrain_updates, self.pretrain_loss], feed_dict={self.x: x})
+        z_h0 = np.random.uniform(low=0, high=1, size=[self.batch_size, self.emb_dim])
+        z_c0 = np.random.uniform(low=0, high=1, size=[self.batch_size, self.emb_dim])
+        feed = {
+            self.h_0: z_h0,
+            self.c_0: z_c0,
+            self.x: x
+        }
+        outputs = sess.run([self.pretrain_updates, self.pretrain_loss], feed_dict=feed)
         return outputs
 
     def init_matrix(self, shape):

models/seqgan/Seqgan.py
@@ -1,21 +1,19 @@
 import json
 from time import time
 
-from utils.metrics.Bleu import Bleu
-from utils.metrics.Nll import Nll
-
 from models.Gan import Gan
 from models.seqgan.SeqganDataLoader import DataLoader, DisDataloader
 from models.seqgan.SeqganDiscriminator import Discriminator
 from models.seqgan.SeqganGenerator import Generator
 from models.seqgan.SeqganReward import Reward
-from utils.metrics.EmbSim import EmbSim
-from utils.oracle.OracleLstm import OracleLstm
-from utils.utils import *
-
-from utils.oracle.OracleCfg import OracleCfg
+from utils.metrics.Bleu import Bleu
 from utils.metrics.Cfg import Cfg
+from utils.metrics.EmbSim import EmbSim
+from utils.metrics.Nll import Nll
+from utils.oracle.OracleCfg import OracleCfg
+from utils.oracle.OracleLstm import OracleLstm
 from utils.text_process import *
+from utils.utils import *
 
 
 class Seqgan(Gan):
@@ -59,7 +57,7 @@ class Seqgan(Gan):
         generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
         self.dis_data_loader.load_train_data(self.oracle_file, self.generator_file)
         for _ in range(3):
-            self.dis_data_loader.reset_pointer()
+            self.dis_data_loader.next_batch()
             x_batch, y_batch = self.dis_data_loader.next_batch()
             feed = {
                 self.discriminator.input_x: x_batch,
@@ -157,8 +155,8 @@ class Seqgan(Gan):
             for _ in range(15):
                 self.train_discriminator()
 
-    def init_cfg_training(self):
-        oracle = OracleCfg(sequence_length=self.sequence_length)
+    def init_cfg_training(self, grammar=None):
+        oracle = OracleCfg(sequence_length=self.sequence_length, cfg_grammar=grammar)
         self.set_oracle(oracle)
         self.oracle.generate_oracle()
         self.vocab_size = self.oracle.vocab_size + 1
@@ -179,12 +177,21 @@ class Seqgan(Gan):
         self.set_data_loader(gen_loader=gen_dataloader, dis_loader=dis_dataloader, oracle_loader=oracle_dataloader)
         return oracle.wi_dict, oracle.iw_dict
 
-    def init_cfg_metric(self):
-        cfg = Cfg(test_file=self.test_file)
+    def init_cfg_metric(self, grammar=None):
+        cfg = Cfg(test_file=self.test_file, cfg_grammar=grammar)
         self.add_metric(cfg)
 
     def train_cfg(self):
-        wi_dict_loc, iw_dict_loc = self.init_cfg_training()
+        cfg_grammar = """
+          S -> S PLUS x | S SUB x |  S PROD x | S DIV x | x
+          PLUS -> '+'
+          SUB -> '-'
+          PROD -> '*'
+          DIV -> '/'
+          x -> 'x' | 'y'
+        """
+
+        wi_dict_loc, iw_dict_loc = self.init_cfg_training(cfg_grammar)
         with open(iw_dict_loc, 'r') as file:
             iw_dict = json.load(file)
 
@@ -194,11 +201,11 @@ class Seqgan(Gan):
             with open(self.test_file, 'w') as outfile:
                 outfile.write(code_to_text(codes=codes, dictionary=dict))
 
-        self.init_cfg_metric()
+        self.init_cfg_metric(grammar=cfg_grammar)
         self.sess.run(tf.global_variables_initializer())
 
-        self.pre_epoch_num = 10
-        self.adversarial_epoch_num = 80
+        self.pre_epoch_num = 0
+        self.adversarial_epoch_num = 200
         self.log = open('experiment-log-seqgan-cfg.csv', 'w')
         # generate_samples(self.sess, self.oracle, self.batch_size, self.generate_num, self.oracle_file)
         generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
@@ -253,5 +260,5 @@ class Seqgan(Gan):
 
 if __name__ == '__main__':
     seqgan = Seqgan()
-    # seqgan.train_oracle()
-    seqgan.train_cfg()
+    seqgan.train_oracle()
+    # seqgan.train_cfg()

models/textGan_MMD/TextganGenerator.py
@@ -1,6 +1,6 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops import tensor_array_ops, control_flow_ops
-import numpy as np
 
 
 class Generator(object):
@@ -155,7 +155,14 @@ class Generator(object):
         return sess.run(self.pretrain_loss, feed)
 
     def pretrain_step(self, sess, x):
-        outputs = sess.run([self.pretrain_updates, self.pretrain_loss], feed_dict={self.x: x})
+        z_h0 = np.random.uniform(low=0, high=1, size=[self.batch_size, self.emb_dim])
+        z_c0 = np.random.uniform(low=0, high=1, size=[self.batch_size, self.emb_dim])
+        feed = {
+            self.h_0: z_h0,
+            self.c_0: z_c0,
+            self.x: x
+        }
+        outputs = sess.run([self.pretrain_updates, self.pretrain_loss], feed_dict=feed)
         return outputs
 
     def init_matrix(self, shape):